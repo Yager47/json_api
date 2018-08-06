7b7d352891c1cc1b766b1f1ffc0b9e900ee54ac7
Sergey Kovalenko <ghostship0704@gmail.com>
2018-08-06 16:47:03 +0300

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+List.find_or_create_by(
+  title: 'West Sweden Road Trip',
+  excerpt: 'A cool road trip with stops in harbors of the coast'
+)
+List.find_or_create_by(
+  title: 'Must have equipment for the outdoor photographer',
+  excerpt: 'My selection of gear for modern outdoor photography'
+)
