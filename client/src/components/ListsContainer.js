980792a86f6cc02869cdf78339e61432791ed297
Sergey Kovalenko <ghostship0704@gmail.com>
2018-08-14 19:03:39 +0300

    edit list action

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import List from './List';
 import NewListForm from './NewListForm';
+import EditListForm from './EditListForm';
 
 class ListsContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      lists: []
+      lists: [],
+      editingListId: null
     }
     this.addNewList = this.addNewList.bind(this)
     this.removeList = this.removeList.bind(this)
+    this.editingList = this.editingList.bind(this)
+    this.editList = this.editList.bind(this)
   }
 
   componentDidMount() {
@@ -29,7 +33,11 @@ class ListsContainer extends Component {
       <div className="lists-container">
         <NewListForm onNewList={this.addNewList} />
         {this.state.lists.map(list => {
-          return (<List list={list} key={list.id} onRemoveList={this.removeList} />)
+          if (this.state.editingListId === list.id) {
+            return(<EditListForm list={list} key={list.id} editList={this.editList} />)
+          } else {
+            return (<List list={list} key={list.id} onRemoveList={this.removeList} editingList={this.editingList} />)
+          }
         })}
       </div>
     )
@@ -45,6 +53,31 @@ class ListsContainer extends Component {
     .catch(error => {
       console.log(error)
     })
+  }
+
+  editingList(id) {
+    this.setState({
+      editingListId: id
+    })
+  }
+
+  editList(id, title, excerpt) {
+    axios.put('/api/v1/lists/' + id, {
+      list: {
+        title,
+        excerpt
+      }
+    })
+    .then(response => {
+      console.log(response);
+      const lists = this.state.lists;
+      lists[id - 1] = {id, title, excerpt}
+      this.setState(() => ({
+        lists,
+        editingListId: null
+      }))
+    })
+    .catch(error => console.log(error));
   }
 
   removeList(id) {
